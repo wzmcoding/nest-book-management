a67295de1254e540223022f890400a17125e142c
wzm <2791063637@qq.com>
2025-02-17 13:41:24 +0800

    feat: Implement user login functionality with validation

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,6 +2,7 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { UserService } from './user.service';
 import { RegisterUserDto } from './dto/register-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Controller('user')
 export class UserController {
@@ -10,5 +11,10 @@ export class UserController {
   @Post('register')
   register(@Body() registerUserDto: RegisterUserDto) {
       return this.userService.register(registerUserDto);
+  }
+
+  @Post('login')
+  login(@Body() loginUserDto: LoginUserDto) {
+    return this.userService.login(loginUserDto);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,37 @@
 /* eslint-disable prettier/prettier */
-
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { RegisterUserDto } from './dto/register-user.dto';
 import { DbService } from 'src/db/db.service';
 import { User } from './entities/user.entity';
-
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Injectable()
 export class UserService {
-  constructor(private dbService: DbService) {}
+  constructor(private dbService: DbService) { }
 
   async register(registerUserDto: RegisterUserDto) {
     const users = await this.dbService.read();
     const findUser = users.find((user) => user.username === registerUserDto.username);
     if (findUser) {
       throw new BadRequestException('用户名已存在');
-    } 
+    }
     const user = new User();
     user.username = registerUserDto.username;
     user.password = registerUserDto.password;
     users.push(user);
     await this.dbService.write(users);
+    return user;
+  }
+
+  async login(loginUserDto: LoginUserDto) {
+    const users: User[] = await this.dbService.read();
+    const user = users.find((user) => user.username === loginUserDto.username);
+    if (!user) {
+      throw new BadRequestException('用户不存在');
+    }
+    if (user.password !== loginUserDto.password) {
+      throw new BadRequestException('密码错误');
+    }
     return user;
   }
 }
